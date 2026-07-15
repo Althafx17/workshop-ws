7084ffa7178770f61690cb87b918ddf581ec4799
althaf <amalthaf111@gmail.com>
2026-07-15 22:49:05 +0530

    chore: disable local browser caching on server and bump HTML query strings to v=3

--- server.py
+++ server.py
@@ -10,10 +10,18 @@ class SafeMIMERequestHandler(http.server.SimpleHTTPRequestHandler):
         # Serve from the directory where this script is located
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+    # Disable conditional requests (prevents 304 and forces 200 OK)
+    def handle_conditional_get(self):
+        return False
+
     # Force override MIME type mappings (fixes Windows registry issues)
     def end_headers(self):
         # Enable CORS just in case
         self.send_header('Access-Control-Allow-Origin', '*')
+        # Force browser to ignore cache and fetch assets fresh
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
         super().end_headers()
 
 # Explicitly register content types to bypass registry lookup
